483c618765217843784729fbca625fff7bf673a0
yangshilei <17751693884@163.com>
2020-03-20 18:20:19 +0800

    rabbitMQ测试

--- ideal-rabbitmq-1010/src/main/java/com/demo/rabbitmq/rocketmq/Producer.java
+++ ideal-rabbitmq-1010/src/main/java/com/demo/rabbitmq/rocketmq/Producer.java
@@ -4,7 +4,9 @@ package com.demo.rabbitmq.rocketmq;
 import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
 import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.BrokerConfig;
 import com.alibaba.rocketmq.common.message.Message;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
 
@@ -15,25 +17,24 @@ import java.util.Scanner;
  * @Author yangshilei
  * @Date 2019-01-22 16:06
  */
+@Slf4j
 public class Producer {
   public static void main(String[] args) throws MQClientException {
-    DefaultMQProducer producer = new DefaultMQProducer("my-group");
+//    final BrokerConfig brokerConfig = new BrokerConfig();
+//    brokerConfig.setNamesrvAddr("localhost:9876");
 
+    DefaultMQProducer producer = new DefaultMQProducer("my-group");
     producer.setNamesrvAddr("localhost:9876");
-    producer.setInstanceName("rmq-instance");
+//    producer.setInstanceName("rmq-instance");
     producer.start();
 
     try {
+
       Message message = new Message("demo-topic","demo-tag","自生产者的启动确认消息".getBytes());
       producer.send(message);
 
-      while (true){
-        String text = new Scanner(System.in).next();
-        Message msg = new Message("demo-topic","demo-tag",text.getBytes());
-        SendResult sendResult = producer.send(msg);
-      }
     }catch (Exception e){
-      System.out.println("生产者发送消息异常"+e);
+      log.info("===={}",e);
     }
 
     producer.shutdown();
