227da5537de712349c67d0146d1efc9d71f319ff
LX_yangshilei <13222031857@189.cn>
2020-07-31 18:30:38 +0800

    gateway模块配置

--- ideal-gateway-1002/src/main/java/com.demo.gateway/GatewayApplication.java
+++ ideal-gateway-1002/src/main/java/com.demo.gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
