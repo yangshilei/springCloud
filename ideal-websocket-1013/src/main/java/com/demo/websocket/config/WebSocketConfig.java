03f9ee86c2810260722244d379f62036aa6fa640
LX_yangshilei <13222031857@189.cn>
2020-09-20 19:45:12 +0800

    websocket模拟一对多后台推送消息到客户端

--- ideal-websocket-1013/src/main/java/com/demo/websocket/config/WebSocketConfig.java
+++ ideal-websocket-1013/src/main/java/com/demo/websocket/config/WebSocketConfig.java
@@ -1,11 +1,35 @@
 package com.demo.websocket.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.session.StandardSessionFacade;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+@Slf4j
 @Configuration
-public class WebSocketConfig {
+public class WebSocketConfig  extends ServerEndpointConfig.Configurator{
+
+    /**
+     * 修改握手信息
+     */
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        StandardSessionFacade ssf = (StandardSessionFacade) request.getHttpSession();
+        if (ssf != null) {
+            HttpSession httpSession = (HttpSession) request.getHttpSession();
+            //关键操作
+            sec.getUserProperties().put("sessionId", httpSession.getId());
+            log.info("获取到的SessionID：" + httpSession.getId());
+        }
+        super.modifyHandshake(sec, request, response);
+
+    }
 
     /**
      * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
