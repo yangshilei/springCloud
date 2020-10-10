45d80d0616a9aed318d9ebff3c94da77602b67d2
LX_yangshilei <13222031857@189.cn>
2020-10-10 09:17:44 +0800

    properties读取文件测试

--- ideal-kafka-1014/src/main/java/com/demo/kafka/controller/TestController.java
+++ ideal-kafka-1014/src/main/java/com/demo/kafka/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.demo.kafka.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.demo.kafka.dto.KafkaTopic;
 import com.demo.kafka.dto.Result;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.PropertiesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.Properties;
 
 @RestController
 @Api("kafka测试接口")
@@ -26,4 +34,13 @@ public class TestController {
         return Result.ok("测试成功");
     }
 
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+//        properties.load(new FileInputStream("C:\\学习\\计算机技术\\我的项目\\springCloud\\ideal-kafka-1014\\src\\main\\resources\\properties.properties"));
+        properties.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream("properties.properties"),"UTF-8"));
+        System.out.println(properties.getProperty("name"));
+        Enumeration<Object> keys = properties.keys();
+        System.out.println(JSONObject.toJSONString(keys));
+        System.out.println("=="+properties.get("age"));
+    }
 }
