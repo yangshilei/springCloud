4fd1f28ec2603632163d7e50b492010695407fe3
LX_yangshilei <13222031857@189.cn>
2020-10-21 22:28:39 +0800

    LinkedBlockingQueue队列原理分析

---
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/threadPool/LinkedBlockingQueueTest.java
@@ -0,0 +1,52 @@
+package com.demo.spring.test.threadPool;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description: LinkedBlockingQueue的特点是先进先出的队列容器，默认是属于无界队列；
+ * @Author: yangshilei
+ * @Date:
+ */
+public class LinkedBlockingQueueTest {
+
+    public static void main(String[] args) {
+        BlockingQueue linkedBlockingQueue  = new LinkedBlockingQueue(4);
+        linkedBlockingQueue.offer("yangshilei");
+        linkedBlockingQueue.offer("tom");
+        linkedBlockingQueue.offer("tom2");
+        boolean tom3 = linkedBlockingQueue.offer("tom3");
+        System.out.println(tom3);
+        boolean tom4 = linkedBlockingQueue.offer("tom4");
+        System.out.println(tom4);
+//        try {
+//            linkedBlockingQueue.put("tom5");
+//        } catch (InterruptedException e) {
+//            System.out.println("linkedBlockingQueue队列已经存满，阻塞不能再此添加");
+//            e.printStackTrace();
+//        }
+
+        // poll取出并删除队列中的消息
+        Object poll1 = linkedBlockingQueue.poll();
+        System.out.println("poll："+poll1.toString());
+
+        // peek取出不会删除队列中的消息
+        Object peek = linkedBlockingQueue.peek();
+        System.out.println("peek："+peek.toString());
+
+        for(int i = 1 ;i <= 5;i++){
+            Object poll = null;
+            try {
+                poll = linkedBlockingQueue.poll(2, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                System.out.println("取值异常"+i);
+
+            }
+            System.out.println(i+":"+poll.toString());
+        }
+
+    }
+
+
+}
