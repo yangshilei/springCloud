9e398c9f5ec256bd70436c82d34448a036fa2eaa
LX_yangshilei <13222031857@189.cn>
2020-11-06 10:14:43 +0800

    单例模式介绍

---
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/java/Singleton.java
@@ -0,0 +1,36 @@
+package com.demo.spring.test.java;
+
+/**
+ * @Description: 单例模式：
+ */
+public class Singleton {
+
+    // 私有构造方法
+    private Singleton(){
+        // 用来记录感知是否进行实例化
+        System.out.println("创建一个懒加载模式且线程安全的单例");
+    }
+
+    private static class SingletonInst{
+        private static Singleton singleton = new Singleton();
+    }
+
+    public static Singleton getSingleton(){
+        return SingletonInst.singleton;
+    }
+
+
+    public static void main(String[] args) {
+        // 第一次获取实例
+        Singleton singleton1 = Singleton.getSingleton();
+        System.out.println("结束1");
+
+        // 第二次获取实例
+        Singleton singleton2 = Singleton.getSingleton();
+        System.out.println("结束2");
+
+        // 第三次获取实例
+        Singleton singleton3 = Singleton.getSingleton();
+        System.out.println("结束3");
+    }
+}
