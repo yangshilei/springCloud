4c30bc530dce92a2c20e33b4a4d80740841e96e6
LX_yangshilei <13222031857@189.cn>
2020-11-10 09:55:09 +0800

    list线程安全集合

---
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/baseThread/集合类/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package com.demo.spring.test.baseThread.集合类;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ArrayListDemo implements Runnable{
+
+    // 线程安全集合
+    private static List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+
+    @Override
+    public void run() {
+        list.add(1);
+        System.out.println(Thread.currentThread().getName()+"---"+list.size());
+    }
+
+    public static void main(String[] args) {
+        ArrayListDemo demo = new ArrayListDemo();
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        for(int i = 0; i<20;i++){
+
+            pool.execute(demo);
+        }
+        pool.shutdown();
+
+    }
+}
