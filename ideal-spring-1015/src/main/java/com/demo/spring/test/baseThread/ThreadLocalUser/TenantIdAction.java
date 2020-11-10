0b54c6f6c25da48158df12e26e2ff72814653940
LX_yangshilei <13222031857@189.cn>
2020-11-10 09:04:32 +0800

    优化

--- ideal-spring-1015/src/main/java/com/demo/spring/test/baseThread/ThreadLocalUser/TenantIdAction.java
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/baseThread/ThreadLocalUser/TenantIdAction.java
@@ -11,8 +11,7 @@ public class TenantIdAction {
 
     // 从上下问获取租户对象，并给租户对象设置id值；id为当前线程的id；
     public void setTenantId(){
-        Tenant tenant = TenantContext.getTenant();
-        tenant.setTenantId(this.getTenantId());
+        TenantContext.getTenant().setTenantId(this.getTenantId());
     }
 
     // 用"租户-线程id"来拼接租户名称
