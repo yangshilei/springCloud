6084170cf1a6d87b8b7e5e09e480ca340ef6c306
LX_yangshilei <13222031857@189.cn>
2020-11-19 14:31:36 +0800

    文件夹读写操作

--- ideal-spring-1015/src/main/java/com/demo/spring/test/java/FileCopy.java
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/java/FileCopy.java
@@ -15,31 +15,41 @@ public class FileCopy {
         String newName = "222.jpg";
 
         InputStream inputStream = null;
+        BufferedInputStream bfi = null;
         OutputStream outputStream = null;
+        BufferedOutputStream bfo = null;
 
         try {
             File file = new File(filePath+fileName);
             inputStream = new FileInputStream(file);
+            bfi = new BufferedInputStream(inputStream);
             System.out.println(file.isDirectory());
             File file2 = new File(filePath+newName);
             outputStream = new FileOutputStream(file2);
+            bfo = new BufferedOutputStream(outputStream);
 
 
             int i = 0;
             if(file.exists()){
                 System.out.println("文件存在");
-                byte[] bytes = new byte[1024];
-                while (inputStream.read(bytes) != -1){
-                    outputStream.write(bytes);
+                byte[] bytes = new byte[102400];
+                while (bfi.read(bytes) != -1){
+                    bfo.write(bytes);
                     inputStream.read(bytes);
                     i++;
                 }
             }
             System.out.println("循环次数="+i);
         }catch (Exception e){
-
+            System.out.println("出现异常");
         }finally {
             try {
+                if(null != bfi){
+                    bfi.close();
+                }
+                if(null != bfo){
+                    bfo.close();
+                }
                 if(null != inputStream){
                     inputStream.close();
                 }
