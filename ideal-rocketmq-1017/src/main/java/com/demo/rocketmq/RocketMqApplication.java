5b3fadcb5512cbb3fcab9f9ab6f707698873eb1e
LX_yangshilei <13222031857@189.cn>
2020-12-15 18:07:13 +0800

    rocketmq代码提交

--- ideal-rocketmq-1017/src/main/java/com/demo/rocketmq/RocketMqApplication.java
+++ ideal-rocketmq-1017/src/main/java/com/demo/rocketmq/RocketMqApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
--- ideal-rocketmq-1017/src/main/java/com/demo/rocketmq/controller/RocketTestController.java
+++ ideal-rocketmq-1017/src/main/java/com/demo/rocketmq/controller/RocketTestController.java
@@ -1,12 +1,14 @@
 package com.demo.rocketmq.controller;
 
 import com.demo.rocketmq.dto.Result;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @Slf4j
 @RestController
 @RequestMapping("/rocketmq")
