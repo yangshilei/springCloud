3082ea119a2c1a38a9824da0051063fd55a0d1e4
LX_yangshilei <13222031857@189.cn>
2021-01-06 17:17:06 +0800

    重入锁，公平锁和非公平锁

---
+++ ideal-spring-1015/src/main/java/com/demo/spring/test/baseThread/FairLockDemo.java
@@ -0,0 +1,41 @@
+package com.demo.spring.test.baseThread;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/** 
+ * @Description: 重入锁：公平和非公平锁 
+ * @Author: yangshilei
+ */
+public class FairLockDemo implements Runnable{
+
+    public static ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        while (true){
+            try {
+                lock.lock();
+                Thread.sleep(1000);
+                System.out.println("线程名称："+Thread.currentThread().getName());
+            }catch (Exception e){
+                System.out.println("异常");
+            }
+            finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FairLockDemo demo = new FairLockDemo();
+
+        Thread thread1 = new Thread(demo,"线程1");
+        Thread thread2 = new Thread(demo,"线程2");
+        Thread thread3 = new Thread(demo,"线程3");
+        Thread thread4 = new Thread(demo,"线程4");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+}
