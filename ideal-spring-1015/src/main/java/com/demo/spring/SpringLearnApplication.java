6fa56dcf92024e1ee8567e62832cbe406d89ff28
LX_yangshilei <13222031857@189.cn>
2021-01-08 18:07:56 +0800

    gateway

--- ideal-spring-1015/src/main/java/com/demo/spring/SpringLearnApplication.java
+++ ideal-spring-1015/src/main/java/com/demo/spring/SpringLearnApplication.java
@@ -2,12 +2,15 @@ package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableAsync // 开启spring异步注解
+@EnableDiscoveryClient
 public class SpringLearnApplication {
 
     public static void main(String[] args) {
