25de23591523f0b40ccb63f88018dd73e9fe786f
LX_yangshilei <13222031857@189.cn>
2021-01-12 20:17:46 +0800

    gateway

---
+++ ideal-gatewaymy-1002/src/main/java/com/example/idealgatewaymy1002/IdealGatewaymy1002Application.java
@@ -0,0 +1,43 @@
+package com.example.idealgatewaymy1002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class IdealGatewaymy1002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdealGatewaymy1002Application.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
