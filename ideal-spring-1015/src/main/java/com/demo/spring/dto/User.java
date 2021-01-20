956c352f4c6c5c849d05db2ed51c9831521423a7
LX_yangshilei <13222031857@189.cn>
2021-01-20 21:45:08 +0800

    定义equals方法

--- ideal-spring-1015/src/main/java/com/demo/spring/dto/User.java
+++ ideal-spring-1015/src/main/java/com/demo/spring/dto/User.java
@@ -10,13 +10,35 @@ public class User {
 
     private String age;
 
+    public User(){}
+
     public User(String username,String age){
         this.username = username;
         this.age = age;
     }
 
+    // 对象排序规则
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return false;
+        if(!(obj instanceof User)) return false;
+        User user = (User)obj;
+        if (this.username.equals(user.username)){
+            return this.age.equals(user.age);
+        }
+        return this.username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.username.hashCode();
+    }
+
+
     @Override
     public String toString () {
         return JSONObject.toJSONString(this);
     }
+
+
 }
