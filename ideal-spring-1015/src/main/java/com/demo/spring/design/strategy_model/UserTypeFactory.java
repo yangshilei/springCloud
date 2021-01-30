a1e07b92282f04d8056704af73fa209b133af6ae
LX_yangshilei <13222031857@189.cn>
2021-01-30 21:12:38 +0800

    策略模式简单演示

--- ideal-spring-1015/src/main/java/com/demo/spring/design/strategy_model/UserTypeFactory.java
+++ ideal-spring-1015/src/main/java/com/demo/spring/design/strategy_model/UserTypeFactory.java
@@ -3,6 +3,10 @@ package com.demo.spring.design.strategy_model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * @Description: 工厂类：可以避免在controller中逐个添加业务实现类
+ * @Author: yangshilei
+ */
 @Component
 public class UserTypeFactory {
 
